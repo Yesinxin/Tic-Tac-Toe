be3589e419be0654993f7ae0a6531283cc844616
roy19851016 <roy19851016@gmail.com>
2024-08-14 12:48:35 +0800

    react練習

--- README.md
+++ README.md
@@ -1,70 +1,22 @@
-# Getting Started with Create React App
+# 井字遊戲 (Tic Tac Toe)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+這是一個使用 React 開發的井字遊戲。遊戲界面簡潔，易於操作，適合所有年齡層的玩家。
 
-## Available Scripts
+## 目錄
 
-In the project directory, you can run:
+- [功能](#功能)
+- [技術棧](#技術棧)
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## 功能
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- 玩家可以選擇 X 或 O 進行遊戲。
+- 顯示當前遊戲狀態（下一位玩家、勝利者或平局）。
+- 提供遊戲歷史，可以隨時跳轉到之前的回合。
+- 清除棋盤以重新開始遊戲。
 
-### `npm test`
+## 技術棧
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+- **前端**: React, JavaScript, CSS
+- **字型**: Google Fonts (Roboto)
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,134 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useCallback, useMemo } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
+const BOARD_SIZE = 3;
+const TOTAL_SQUARES = BOARD_SIZE * BOARD_SIZE;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Square({ value, onClick, isWinning }) {
+  return (
+    <button 
+      className={`square ${isWinning ? 'winning' : ''}`} 
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board({ squares, onClick, winningLine }) {
+  const renderSquare = useCallback((i) => {
+    return (
+      <Square
+        key={i}
+        value={squares[i]}
+        onClick={() => onClick(i)}
+        isWinning={winningLine && winningLine.includes(i)}
+      />
+    );
+  }, [squares, onClick, winningLine]);
+
+  return (
+    <div>
+      {[...Array(BOARD_SIZE)].map((_, row) => (
+        <div key={row} className="board-row">
+          {[...Array(BOARD_SIZE)].map((_, col) => renderSquare(row * BOARD_SIZE + col))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Game() {
+  const [history, setHistory] = useState([Array(TOTAL_SQUARES).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = useCallback((squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return { winner: squares[a], line: lines[i] };
+      }
+    }
+    return squares.every(Boolean) ? { winner: 'Draw' } : null;
+  }, []);
+
+  const current = history[stepNumber];
+  const winInfo = useMemo(() => calculateWinner(current), [current, calculateWinner]);
+
+  const handleClick = useCallback((i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const currentSquares = [...newHistory[newHistory.length - 1]];
+    if (calculateWinner(currentSquares) || currentSquares[i]) {
+      return;
+    }
+    currentSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...newHistory, currentSquares]);
+    setStepNumber(newHistory.length);
+    setXIsNext(!xIsNext);
+  }, [history, stepNumber, xIsNext, calculateWinner]);
+
+  const jumpTo = useCallback((step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }, []);
+
+  const clearBoard = useCallback(() => {
+    setHistory([Array(TOTAL_SQUARES).fill(null)]);
+    setStepNumber(0);
+    setXIsNext(true);
+  }, []);
+
+  const moves = useMemo(() => history.map((_, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)} className={move === stepNumber ? 'current-move' : ''}>
+          {desc}
+        </button>
+      </li>
+    );
+  }), [history, jumpTo, stepNumber]);
+
+  let status;
+  if (winInfo?.winner === 'Draw') {
+    status = "Game ended in a draw";
+  } else if (winInfo?.winner) {
+    status = "Winner: " + winInfo.winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+      <div className="game">
+        <div className="game-board">
+          <div className="status">{status}</div>
+          <Board
+            squares={current}
+            onClick={handleClick}
+            winningLine={winInfo?.line}
+          />
+          <button onClick={clearBoard} className="clear-button">Clear Board</button>
+        </div>
+        <div className="game-info">
+          <h3>Game History</h3>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
